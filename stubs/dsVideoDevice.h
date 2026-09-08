a54edaaa8bb0f9afb8fccec520007519ced21108
IshvarKR <130352532+IshvarKR@users.noreply.github.com>
2026-09-08 18:21:12 +0530

    Add dsVideoDevice.h header file with video device APIs

---
+++ stubs/dsVideoDevice.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "dsVideoPort.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    dsVIDEO_ZOOM_NONE = 0,
+    dsVIDEO_ZOOM_FULL,
+    dsVIDEO_ZOOM_16_9_ZOOM
+} dsVideoZoom_t;
+
+typedef enum {
+    dsVIDEO_CODEC_UNKNOWN = 0,
+    dsVIDEO_CODEC_MPEG2,
+    dsVIDEO_CODEC_MPEG4PART10,
+    dsVIDEO_CODEC_H265,
+    dsVIDEO_CODEC_VP9,
+    dsVIDEO_CODEC_AV1
+} dsVideoCodingFormat_t;
+
+typedef struct {
+    uint32_t profiles;
+    uint32_t levels;
+} dsVideoCodecInfo_t;
+
+typedef void (*dsVideoDeviceFramerateCallback_t)(char*);
+
+dsError_t dsVideoDeviceInit(void);
+dsError_t dsVideoDeviceTerm(void);
+dsError_t dsGetVideoDevice(int, intptr_t*);
+dsError_t dsSetDFC(intptr_t, dsVideoZoom_t);
+dsError_t dsGetHDRCapabilities(intptr_t, int*);
+dsError_t dsGetSupportedVideoCodingFormats(intptr_t, unsigned int*);
+dsError_t dsGetVideoCodecInfo(intptr_t, dsVideoCodingFormat_t, dsVideoCodecInfo_t*);
+dsError_t dsForceDisableHDRSupport(intptr_t, bool);
+dsError_t dsSetFRFMode(intptr_t, int);
+dsError_t dsGetFRFMode(intptr_t, int*);
+dsError_t dsGetCurrentDisplayframerate(intptr_t, char*);
+dsError_t dsSetDisplayframerate(intptr_t, char*);
+dsError_t VideoDeviceRegisterFrameratePreChangeCB(dsVideoDeviceFramerateCallback_t);
+dsError_t VideoDeviceRegisterFrameratePostChangeCB(dsVideoDeviceFramerateCallback_t);
+dsError_t dsHdmiInSelectZoomMode(dsVideoZoom_t);
+
+#ifdef __cplusplus
+}
+#endif
