dbbc049fdc53512a10c49fafe81896af282531a4
IshvarKR <130352532+IshvarKR@users.noreply.github.com>
2026-09-08 18:22:45 +0530

    Refactor dsUtl.h by removing unused code
    Removed unused includes and utility functions related to string validation.

--- stubs/dsUtl.h
+++ stubs/dsUtl.h
@@ -1,29 +1,4 @@
 #pragma once
 
-#include <stddef.h>
-#include <stdint.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-typedef enum dsUtlStatus {
-    dsUTL_SUCCESS = 0,
-    dsUTL_ERROR = -1
-} dsUtlStatus_t;
-
-#define DSUTL_MAX_STRING_LENGTH 256
-
-static inline int dsUtlIsNullOrEmpty(const char* value)
-{
-    return (value == NULL) || (value[0] == '\0');
-}
-
-static inline int dsUtlIsValidString(const char* value)
-{
-    return (value != NULL) && (value[0] != '\0');
-}
-
-#ifdef __cplusplus
-}
-#endif
+#include "dsError.h"
+#include "dsTypes.h"
