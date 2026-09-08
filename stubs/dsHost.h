c85f1aff29585c8db866c4d5785bafe2f25aa8e0
IshvarKR <130352532+IshvarKR@users.noreply.github.com>
2026-09-08 18:20:21 +0530

    Refactor dsHost.h by removing unused types and adding functions
    Removed unused enum and struct definitions, added function declarations for dsHostInit, dsHostTerm, and dsGetHostEDID.

--- stubs/dsHost.h
+++ stubs/dsHost.h
@@ -1,32 +1,14 @@
 #pragma once
 
-#include <stdint.h>
+#include "dsError.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef enum dsHostEvent {
-    dsHOST_EVENT_NONE = 0,
-    dsHOST_EVENT_CONNECTED,
-    dsHOST_EVENT_DISCONNECTED,
-    dsHOST_EVENT_MAX
-} dsHostEvent_t;
-
-typedef struct dsHostInfo {
-    uint32_t version;
-    char name[64];
-} dsHostInfo_t;
-
-static inline int dsHostInit(void) { return 0; }
-static inline int dsHostTerm(void) { return 0; }
-static inline int dsGetHostEDID(void* handle, unsigned char* edid, int* length)
-{
-    (void)handle;
-    (void)edid;
-    (void)length;
-    return 0;
-}
+dsError_t dsHostInit(void);
+dsError_t dsHostTerm(void);
+dsError_t dsGetHostEDID(unsigned char*, int*);
 
 #ifdef __cplusplus
 }
