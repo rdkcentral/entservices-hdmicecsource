e9ed9ae04dd995f343727e84b8f94fe87f10fffe
IshvarKR <130352532+IshvarKR@users.noreply.github.com>
2026-09-08 18:58:33 +0530

    Update dsError.h

--- stubs/dsError.h
+++ stubs/dsError.h
@@ -6,6 +6,7 @@ typedef int dsError_t;
 static const dsError_t dsERR_NONE = 0;
 static const dsError_t dsERR_GENERAL = 1;
 static const dsError_t dsERR_INVALID_PARAM = 2;
-static const dsError_t dsERR_OPERATION_NOT_SUPPORTED = 3;
+static const dsError_t dsERR_INVALID_STATE = 3;
+static const dsError_t dsERR_OPERATION_NOT_SUPPORTED = 4;
 
 #endif
