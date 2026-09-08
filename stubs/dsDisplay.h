28f4722c0c6274e0f207603f33adab2e72a95d56
IshvarKR <130352532+IshvarKR@users.noreply.github.com>
2026-09-08 18:18:27 +0530

    Expand dsDisplay.h with enums and structs
    Added definitions for display events, content types, and EDID structure.

--- stubs/dsDisplay.h
+++ stubs/dsDisplay.h
@@ -1,12 +1,63 @@
-/**
- * Mock header for dsDisplay.h
- * Created to resolve compilation errors when devicesettings is not available
- */
-
 #pragma once
 
-#include <cstdint>
+#include "dsError.h"
+#include "dsTypes.h"
 
-// This file is a placeholder mock for dsDisplay.h
-// The actual display event types are defined in host.hpp
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+	dsDISPLAY_EVENT_CONNECTED = 0,
+	dsDISPLAY_EVENT_DISCONNECTED,
+	dsDISPLAY_RXSENSE_ON,
+	dsDISPLAY_RXSENSE_OFF,
+	dsDISPLAY_HDCPPROTOCOL_CHANGE,
+	dsDISPLAY_EVENT_MAX
+} dsDisplayEvent_t;
+
+typedef enum {
+	dsAVICONTENT_TYPE_NOT_SIGNALLED = 0,
+	dsAVICONTENT_TYPE_GRAPHICS,
+	dsAVICONTENT_TYPE_PHOTO,
+	dsAVICONTENT_TYPE_CINEMA,
+	dsAVICONTENT_TYPE_GAME
+} dsAviContentType_t;
+
+typedef enum {
+	dsAVI_SCAN_TYPE_NO_DATA = 0,
+	dsAVI_SCAN_TYPE_OVERSCAN,
+	dsAVI_SCAN_TYPE_UNDERSCAN
+} dsAVIScanInformation_t;
+
+typedef struct {
+	uint16_t productCode;
+	uint32_t serialNumber;
+	uint16_t manufactureYear;
+	uint8_t manufactureWeek;
+	char monitorName[64];
+	bool hdmiDeviceType;
+	bool isRepeater;
+	uint8_t physicalAddressA;
+	uint8_t physicalAddressB;
+	uint8_t physicalAddressC;
+	uint8_t physicalAddressD;
+	uint8_t numOfSupportedResolution;
+} dsDisplayEDID_t;
+
+typedef void (*dsDisplayEventCallback_t)(int, dsDisplayEvent_t, void*);
+
+dsError_t dsDisplayInit(void);
+dsError_t dsDisplayTerm(void);
+dsError_t dsIsDisplayConnected(intptr_t, bool*);
+dsError_t dsGetDisplaySurroundMode(intptr_t, int*);
+dsError_t dsGetEDIDBytes(intptr_t, unsigned char*, int*);
+dsError_t dsGetDisplay(dsVideoPortType_t, int, intptr_t*);
+dsError_t dsGetDisplayAspectRatio(intptr_t, dsVideoAspectRatio_t*);
+dsError_t dsGetEDID(intptr_t, dsDisplayEDID_t*);
+dsError_t dsRegisterDisplayEventCallback(intptr_t, dsDisplayEventCallback_t);
+
+#ifdef __cplusplus
+}
+#endif
 
