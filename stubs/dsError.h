328521121d05e79c0664ca94360f5a2d28e6ccce
IshvarKR <130352532+IshvarKR@users.noreply.github.com>
2026-09-08 18:18:58 +0530

    Add new error codes for invalid parameters and unsupported operations

--- stubs/dsError.h
+++ stubs/dsError.h
@@ -5,5 +5,7 @@ typedef int dsError_t;
 
 static const dsError_t dsERR_NONE = 0;
 static const dsError_t dsERR_GENERAL = 1;
+static const dsError_t dsERR_INVALID_PARAM = 2;
+static const dsError_t dsERR_OPERATION_NOT_SUPPORTED = 3;
 
 #endif
